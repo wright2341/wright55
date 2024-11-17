87461fd4e78800e99596dbd7848f235476a25a9d
wright2341 <wright.amedu@binghamuni.edu.ng>
2024-11-18 00:14:05 +0100

    Add files via upload

---
+++ wright.py
@@ -0,0 +1,43 @@
+def calculate_speed():
+    distance = float(input("Enter distance"))
+    time = float(input("Enter time"))
+    # speed = distance / time
+    print(distance/time)
+
+calculate_speed();
+
+def calculate_force():
+    mass = float(input("Enter time"))
+    acceleration = float(input("Enter acceleration"))
+    print(mass*acceleration)
+
+calculate_force();
+
+def calculate_momentum():
+    mass = float(input("Enter mass"))
+    velocity = float(input("Enter velocity"))
+    print(mass*velocity)
+
+calculate_momentum();
+
+def calculate_energy():
+    power = float(input("Enter power"))
+    time = float(input("Enter time"))
+    print(power*time)
+
+calculate_energy();
+
+def calculate_distance():
+
+    speed = float(input(" Enter speed "))
+    time = float(input(" Enter time "))
+    print(speed * time)
+
+calculate_distance();
+
+
+
+
+
+
+
